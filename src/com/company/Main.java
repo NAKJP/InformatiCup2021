d6062e5ed2534b938d14857cb58118cff6482622
Julian Texo <70061244+NAKJP@users.noreply.github.com>
2020-11-12 18:54:32 +0100

    Added Programm

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        establishConnection();
+    }
+
+    public static void establishConnection(){
+        String apiKey = "4TB4RVHI6UZ4NQRIV4IDZYUERICKBWQMRMLSD5NVY756YYM5S3ZMJN2P";
+        try {
+            // open websocket
+            final Websocket clientEndPoint = new Websocket(new URI("wss://msoll.de/spe_ed?key="+apiKey));
+
+            // add listener
+            clientEndPoint.addMessageHandler(new Websocket.MessageHandler() {
+                public void handleMessage(String message) {
+                    System.out.println(message);
+                }
+            });
+
+            clientEndPoint.sendMessage("{'action': 'change_nothing'}");
+
+            Thread.sleep(300000);
+
+        }catch (InterruptedException ex) {
+            System.err.println("InterruptedException exception: " + ex.getMessage());
+        }catch (URISyntaxException ex) {
+            System.err.println("URISyntaxException exception: " + ex.getMessage());
+        }
+    }
+
+}
---
+++ src/com/company/Websocket.java
@@ -0,0 +1,72 @@
+package com.company;
+
+import java.net.URI;
+import javax.websocket.ClientEndpoint;
+import javax.websocket.CloseReason;
+import javax.websocket.ContainerProvider;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+
+@ClientEndpoint
+public class Websocket {
+
+    Session userSession = null;
+    private MessageHandler messageHandler;
+
+    public Websocket (URI endpointURI){
+        try{
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            container.connectToServer(this, endpointURI);
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @OnOpen
+    public void onOpen(Session userSession) {
+        System.out.println("opening websocket");
+        this.userSession = userSession;
+    }
+
+    @OnClose
+    public void onClose(Session userSession, CloseReason reason) {
+        System.out.println("closing websocket");
+        this.userSession = null;
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        if (this.messageHandler != null) {
+            this.messageHandler.handleMessage(message);
+        }
+    }
+
+    /**
+     * register message handler
+     *
+     * @param msgHandler
+     */
+    public void addMessageHandler(MessageHandler msgHandler) {
+        this.messageHandler = msgHandler;
+    }
+
+
+    public void sendMessage(String message) {
+        this.userSession.getAsyncRemote().sendText(message);
+    }
+
+
+    /**
+     * Message handler.
+     *
+     * @author Jiji_Sasidharan
+     */
+    public static interface MessageHandler {
+        public void handleMessage(String message);
+    }
+}
+
+
